ef3bba7a105b5c6884dc61b982b4d8424e05706b
Hungryhunter96 <ankur96xyz@gmail.com>
2019-10-17 21:15:41 +0530

    Update paytm_config.js

--- paytm/paytm_config.js
+++ paytm/paytm_config.js
@@ -1,9 +1,9 @@
 module.exports = {
   paytm_config: {
-		MID: '',
-		WEBSITE: '',
-    CHANNEL_ID: '',
-    INDUSTRY_TYPE_ID: '',
-    MERCHANT_KEY : ''
+		MID: 'copYkQ20143736243713',
+		WEBSITE: 'DEFAULT',
+    CHANNEL_ID: 'WAP',
+    INDUSTRY_TYPE_ID: 'Retail',
+    MERCHANT_KEY : 'uYGpxYW9LM9G7IBH'
 	}
 }
